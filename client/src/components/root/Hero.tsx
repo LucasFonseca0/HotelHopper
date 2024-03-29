7be5d97d983ae19dd17327a352908ebee119ed91
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-03-29 13:42:25 +0000

    fix: fixed responsive problem on hero

--- client/src/components/root/Hero.tsx
+++ client/src/components/root/Hero.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <div className="w-full h-[92vh] banner flex items-center pl-8 sm:pl-16 md:pl-32 z-10 absolute">
+    <div className="w-full h-[92vh] banner flex items-center justify-center sm:justify-start sm:pl-16 md:pl-32 z-10 absolute">
 
       
-      <div className="w-[70%] max-w-[70rem] p-2  pl-8 text-black rounded-[8px] shadow  bg-white  
+      <div className="w-[90%] sm:w-[70%] max-w-[70rem] p-2  pl-8 text-black rounded-[8px] shadow  bg-white  
        ">
-        <h1 className="text-3xl py-5 xl:text-6xl font-[700] xl:leading-[80px] sm:mt-20 font-Inter">
+        <h1 className="text-3xl py-5 xl:text-6xl font-[700] xl:leading-[80px] sm:mt-4 md:mt-12 font-Inter">
           Luxurious and comfortable hotels, <br />
           Booked straight from your home!
         </h1>
