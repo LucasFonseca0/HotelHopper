0bef10294ce877f97837962bd0fe2aa4cb44c66f
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-03-29 12:38:10 +0000

    fix: spinner label fixed

--- client/src/shared/Loading/LoadingSpinner.tsx
+++ client/src/shared/Loading/LoadingSpinner.tsx
@@ -5,7 +5,7 @@ const LoadingSpinner = () => {
   return (
 
     <div className="absolute inset-0 flex justify-center items-center z-50 bg-gray-900 bg-opacity-50 h-full">
-    <Spinner label="Secondary" color="secondary" labelColor="secondary" />
+    <Spinner label="loading..." color="secondary" labelColor="secondary" size='lg' className='font-bold'/>
   </div>
     
   );
